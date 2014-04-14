f6b33e2c07194100af1ed013bc9f6460c9f6e6af
Francesco Pisani <tlhingan@ARIS177380.(none)>
2014-04-14 15:25:38 -0600

    Release HelloWorld

--- HelloWorld/platforms/android/ant-gen/R.java.d
+++ HelloWorld/platforms/android/ant-gen/R.java.d
@@ -1,14 +1,9 @@
 /home/tlhingan/Git/PhoneGap/HelloWorld/platforms/android/ant-gen/com/phonegap/helloworld/R.java \
  : /home/tlhingan/Git/PhoneGap/HelloWorld/platforms/android/res/drawable-ldpi/icon.png \
-/home/tlhingan/Git/PhoneGap/HelloWorld/platforms/android/res/drawable/icon.png \
-/home/tlhingan/Git/PhoneGap/HelloWorld/platforms/android/res/values/strings.xml \
 /home/tlhingan/Git/PhoneGap/HelloWorld/platforms/android/res/drawable-xhdpi/icon.png \
-/home/tlhingan/Git/PhoneGap/HelloWorld/platforms/android/res/xml/config.xml \
-/home/tlhingan/Git/PhoneGap/HelloWorld/platforms/android/res/drawable-hdpi/icon.png \
 /home/tlhingan/Git/PhoneGap/HelloWorld/platforms/android/res/drawable-mdpi/icon.png \
-/home/tlhingan/Git/PhoneGap/HelloWorld/platforms/android/ant-build/res/drawable-ldpi/icon.png \
-/home/tlhingan/Git/PhoneGap/HelloWorld/platforms/android/ant-build/res/drawable/icon.png \
-/home/tlhingan/Git/PhoneGap/HelloWorld/platforms/android/ant-build/res/drawable-xhdpi/icon.png \
-/home/tlhingan/Git/PhoneGap/HelloWorld/platforms/android/ant-build/res/drawable-hdpi/icon.png \
-/home/tlhingan/Git/PhoneGap/HelloWorld/platforms/android/ant-build/res/drawable-mdpi/icon.png \
+/home/tlhingan/Git/PhoneGap/HelloWorld/platforms/android/res/drawable/icon.png \
+/home/tlhingan/Git/PhoneGap/HelloWorld/platforms/android/res/xml/config.xml \
+/home/tlhingan/Git/PhoneGap/HelloWorld/platforms/android/res/values/strings.xml \
+/home/tlhingan/Git/PhoneGap/HelloWorld/platforms/android/res/drawable-hdpi/icon.png \
 /home/tlhingan/Git/PhoneGap/HelloWorld/platforms/android/ant-build/AndroidManifest.xml \
